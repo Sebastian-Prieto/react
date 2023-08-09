f04a242e7023c96a9d1f4e4cda43a89bcf04b5a3
Sebastian <sbprietoc@gmail.com>
2023-08-08 19:37:51 -0500

    Add component

---
+++ ReactBasicComponent/projects/HolaMundo/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+export function App () {
+    return (
+        <article>
+            <header>
+                <img src="https://pbs.twimg.com/profile_images/1671749988433510402/ztLuSa4s_400x400.jpg" alt="Sebastián's avatar" />
+            <div>
+                <strong>Sebastián Prieto</strong>
+                <span>@oliversyklones</span>
+            </div>
+            </header>
+            <aside>
+                <button>Follow</button>
+            </aside>
+        </article>
+    )
+}
